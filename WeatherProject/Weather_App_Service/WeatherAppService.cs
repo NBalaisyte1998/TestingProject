fd601dd698905f6ed835a9d025734b840d0d92db
Norberta Balaisyte <NBalaisyte@spartaglobal.com>
2019-10-09 13:36:44 +0100

    revised model

--- WeatherProject/Weather_App_Service/WeatherAppService.cs
+++ WeatherProject/Weather_App_Service/WeatherAppService.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using WeatherProject.Weather_App_Service.HTTPS_Manager;
+using WeatherProject.Weather_App_Service.Data_Handling;
 
 namespace WeatherProject.Weather_App_Service
 {
-    class Class1
+    class WeatherAppService
     {
+        public WeatherAppDTO weatherAppDTO = new WeatherAppDTO();
+        public WeatherAppService weatherAppService = new WeatherAppService();
+        public JObject WeatherJSon;
+
+        public WeatherAppManager weatherAppManager = new WeatherAppManager();
+
+
+        public WeatherAppService()  
+        {
+            weatherAppDTO.DeserializeWeatherApp(WeatherManager.);
+            WeatherJSon = JObject.Parse(WeatherManager.GetLatestRates());
+        }
     }
 }
